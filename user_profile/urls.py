8771c95fe155c2e364f4ad25057acc4404338d80
astro81 <caien720@protonmail.com>
2024-04-13 19:11:14 +0545

    user profile deletee

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from user_profile.views import UserProfileView
+from user_profile.views import UserProfileView, UserDeleteProfileView
 
 urlpatterns = [
     path('profile/<str:username>/', UserProfileView.as_view(), name='profile'),
+    path('profile/<str:username>/delete/', UserDeleteProfileView.as_view(), name='delete-profile'),
 ]
 
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,7 @@
-from rest_framework.generics import get_object_or_404, RetrieveUpdateAPIView
+from rest_framework import status
+from rest_framework.generics import get_object_or_404, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from user_profile.models import UserProfile
 from user_profile.permissions import IsOwnerOrReadOnly
@@ -23,3 +25,25 @@ class UserProfileView(RetrieveUpdateAPIView):
     def put(self, request, *args, **kwargs):
         return Response({'detail': 'Put update not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+class UserDeleteProfileView(DestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        return UserProfile.objects.all()
+
+    def get_object(self):
+        username = self.kwargs.get('username')
+        profile = get_object_or_404(UserProfile, user__username=username)
+
+        self.check_object_permissions(self.request, profile)
+
+        return profile
+
+    def destroy(self, request, *args, **kwargs):
+        profile = self.get_object()
+        user = profile.user
+        profile.delete()
+        user.delete()
+
+        return Response({'detail': 'Delete profile success'}, status=status.HTTP_204_NO_CONTENT)
