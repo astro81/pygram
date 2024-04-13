16f95d901bfbbb43d3378313ab06334ea7f5acb7
astro81 <caien720@protonmail.com>
2024-04-13 11:48:23 +0545

    user profile update

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,18 +1,25 @@
-from rest_framework import status
-from rest_framework.generics import RetrieveAPIView, get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import get_object_or_404, RetrieveUpdateAPIView
 from rest_framework.response import Response
 
 from user_profile.models import UserProfile
+from user_profile.permissions import IsOwnerOrReadOnly
 from user_profile.serializers import ProfileSerializer
 
-
-# Create your views here.
-class UserProfileView(RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
+class UserProfileView(RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        return UserProfile.objects.all()
 
     def get_object(self):
         username = self.kwargs.get('username')
-        return get_object_or_404(UserProfile, user__username=username)
+        profile = get_object_or_404(UserProfile, user__username=username)
+
+        self.check_object_permissions(self.request, profile)
+
+        return profile
+
+    def put(self, request, *args, **kwargs):
+        return Response({'detail': 'Put update not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
